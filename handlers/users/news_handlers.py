d37944c7235273965f3054e2d6056b136cede17a
serg-sh <serg.shp@gmail.com>
2021-10-21 17:28:20 +0300

    карусель новостей

--- handlers/users/news_handlers.py
+++ handlers/users/news_handlers.py
@@ -15,31 +15,52 @@ def language_insert(user_language, api_link):
 
 
 @dp.callback_query_handler(text_contains='show_news')
-async def show_news(call: CallbackQuery):
-    await call.message.edit_reply_markup()
+async def show_news(call: CallbackQuery, news_id: int = None):
     user_language = await db.get_language()
     all_news = await db.get_all_news()
+    list_id_news = [news.id for news in all_news]
     news = all_news[len(all_news) - 1]
+    if news_id:
+        news = await db.get_news(news_id=news_id)
     url_news = language_insert(user_language, news.api_link)
     text = f'<b>{news.title}</b>\n' \
            f'{url_news}\n' \
-           f'{news.id}'  # убрать
-    await call.message.answer(text=text, reply_markup=ukb.markup_news)
+           f'ИД: {news.id}'
+    await call.message.edit_text(text=text, reply_markup=ukb.markup_news)
 
 
 @dp.callback_query_handler(text_contains='next_news')
 async def next_news(call: CallbackQuery):
-    user_language = await db.get_language()
+    news_id = get_news_id(call) - 1
     all_news = await db.get_all_news()
+    list_id_news = [news.id for news in all_news]
+    while True:
+        if await db.get_news(news_id=news_id):
+            await show_news(call, news_id=news_id)
+            break
+        news_id -= 1
+        if news_id < list_id_news[0]:
+            await show_news(call)
+            return
 
-    news = all_news[len(all_news) - 2]
-    url_news = language_insert(user_language, news.api_link)
-    text = f'<b>{news.title}</b>\n' \
-           f'{url_news}\n' \
-           f'{news.id}'  # убрать
-    await call.message.edit_text(text=text, reply_markup=ukb.markup_news)
+
+def get_news_id(call):
+    text = call.message.text
+    text_id = text.find('ИД:')
+    news_id = int(text[text_id + 4:])
+    return news_id
 
 
 @dp.callback_query_handler(text_contains='prev_news')
 async def prev_news(call: CallbackQuery):
-    await call.message.answer(text='123')
+    news_id = get_news_id(call) + 1
+    all_news = await db.get_all_news()
+    list_id_news = [news.id for news in all_news]
+    while True:
+        if news_id > list_id_news[len(list_id_news) - 1]:
+            await show_news(call, news_id=list_id_news[0])
+            return
+        if await db.get_news(news_id=news_id):
+            await show_news(call, news_id=news_id)
+            break
+        news_id += 1
