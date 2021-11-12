2fae92f16dc76b543ada7916184dac59f2b4523f
serg-sh <serg.shp@gmail.com>
2021-11-12 20:30:14 +0200

    - config - добавленны доанные для отправки заявки - добавлен апи запрос для создания заявки

--- utils/http_api/site_api.py
+++ utils/http_api/site_api.py
@@ -1,3 +1,4 @@
+import json
 from typing import List
 
 import requests
@@ -9,16 +10,29 @@ db = database.DBCommands()
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (HTML, like Gecko) "
-                  "Chrome/47.0.2526.106 Safari/537.36"
+                  "Chrome/47.0.2526.106 Safari/537.36",
+    "Content-Type": "application/json",
 }
-
-news_url = config.api_url_news_ru
 
 
 async def get_news() -> List:
-    request = requests.get(news_url, headers=headers)
+    request = requests.get(config.api_url_news_ru, headers=headers)
     data = request.json()
     list_news = data.get('rows')
     for news in list_news[::-1]:
         await db.add_new_news(news)
     return list_news
+
+
+def send_order(order_data: dict):
+    """
+  order_data = {'email': 'test@mail',
+                'phone': '+380971112233',
+                'company': 'Рога и Копыта',
+                'fio': 'Иванов ИИ',
+                'comment': 'Комментарий'}
+    """
+    json_data = json.dumps(order_data)
+    request = requests.post(url=config.api_order_url, data=json_data, headers=headers,
+                            auth=(config.api_user, config.api_pass))
+    return request.status_code
